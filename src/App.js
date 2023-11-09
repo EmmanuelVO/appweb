3529bad937ea43714963941f1680fc412eb9d74f
EmmanuelVO <83371337+EmmanuelVO@users.noreply.github.com>
2023-11-09 12:24:01 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import analyzeimage from './analyzeimage';
 
 function App() {
   const title = 'Titulo chingon';
@@ -9,7 +9,7 @@ function App() {
     <div>
       <h1>{title}</h1>
       <input type="text" />
-
+      <button onClick={() => analyzeimage('https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png')}>Analizar</button>
       <button>Generar</button>
 
       
