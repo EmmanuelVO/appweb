5863c6255b6d99d177e535eada5960417fedf450
EmmanuelVO <83371337+EmmanuelVO@users.noreply.github.com>
2023-11-09 12:06:12 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+#import analyzeimage from './analyzeimage';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const title = 'Titulo chingon';
+  
+  return (
+
+    <div>
+      <h1>{title}</h1>
+      <input type="text" />
+      <button onClick={() => analyzeimage('https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png')}>Analizar</button>
+      <button>Generar</button>
+
+      
+    </div>
+  );
+
+
 }
 
 export default App;
