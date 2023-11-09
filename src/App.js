16441da3232dce18c5e3fd598d058fbc540746aa
EmmanuelVO <83371337+EmmanuelVO@users.noreply.github.com>
2023-11-09 12:37:48 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import analyzeimage from './analyzeimage';
 
 function App() {
-  const title = 'Titulo chingon';
+  const title = 'checar';
   
   return (
 
     <div>
       <h1>{title}</h1>
       <input type="text" />
-      <button onClick={() => analyzeimage('https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png')}>Analizar</button>
+      <button onClick={() => analyzeimage()}>Analizar</button>
       <button>Generar</button>
 
       
